668487c314e58f940ce5164130e4b16c54f76832
chaimaef <chaimaefahmi@outlook.com>
2018-11-21 12:16:54 -0500

    Modified Ranking

---
+++ interCityRideSharingSystem-Web/src/components/firstRanking.js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+var config = require('../../config')
+
+var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+var AXIOS = axios.create({
+  baseURL: backendUrl,
+  headers: { 'Access-Control-Allow-Origin': frontendUrl }
+})
+  
+  export default {
+    name: 'DriverRanking',
+    data () {
+        return {
+          participants: [],
+          newParticipant: '',
+          errorParticipant: '',
+          response: []
+        }     
+    }, 
+    created: function () {
+        // Initializing participants from backend
+          AXIOS.get(`/rankDrivers`)
+          .then(response => {
+            // JSON responses are automatically parsed.
+            this.participants = response.data
+          })
+          .catch(e => {
+            this.errorParticipant = e;
+          });
+      }, 
+      methods: {
+        rankDrivers: function () {
+            AXIOS.post(`/rankDrivers`, {}, {})
+            .then(response => {
+              // JSON responses are automatically parsed.
+              this.participants.push(response.data)
+              this.newParticipant = ''
+              this.errorParticipant = ''
+            })
+            .catch(e => {
+              var errorMsg = e.message
+              console.log(errorMsg)
+              this.errorParticipant = errorMsg
+            });
+          }
+      }
+  }
+
