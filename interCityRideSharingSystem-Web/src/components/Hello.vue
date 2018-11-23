2ee2756893dc110b55e936afdffc7ef412f5ae8b
chaimaef <chaimaefahmi@outlook.com>
2018-11-22 19:24:43 -0500

    Closes #16

--- interCityRideSharingSystem-Web/src/components/Hello.vue
+++ interCityRideSharingSystem-Web/src/components/Hello.vue
@@ -1,30 +1,16 @@
 <template>
   <div class="hello">
+  <br>
     <h1>{{ msg }}</h1>
     <h2>Support Services</h2>
     <ul>
+  <br>
      <li><a href="#/status"><button>Fleet Status Overview</button></a></li>
       <br>
       <br>
       <li><a href="#/ranking"><button>Rankings Overview</button></a></li>
        </ul>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+     </div>
 </template>
 
 <script>
